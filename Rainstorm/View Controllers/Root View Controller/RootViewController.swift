fbe750ac81d875ebd934b99e30a0e7b51f4deca7
dennisvera <dennisvera.z@gmail.com>
2018-07-17 18:10:20 -0400

    Project setup

---
+++ Rainstorm/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RootViewController.swift
+//  Rainstorm
+//
+//  Created by Dennis Vera on 7/17/18.
+//  Copyright © 2018 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
+
