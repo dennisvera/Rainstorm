82696f129de508af9ec3ea240e397471920063f5
dennisvera <dennisvera.z@gmail.com>
2018-07-17 20:41:32 -0400

    Day and Week View controllers setup and instantiating.

---
+++ Rainstorm/View Controllers/Weather View Controllers/Day View Controller/DayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DayViewController.swift
+//  Rainstorm
+//
+//  Created by Dennis Vera on 7/17/18.
+//  Copyright © 2018 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+final class DayViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup View
+        setupView()
+    }
+    
+    // MARK: - Helper Method
+    
+    private func setupView() {
+        // Configure View
+        view.backgroundColor = .blue
+    }
+    
+}
