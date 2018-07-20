5585a49c9b2dce3f7ea1997b4a1a57932efcff66
dennisvera <dennisvera.z@gmail.com>
2018-07-20 13:15:23 -0400

    Adopted weather data protocol to de-couple root view contorller.

---
+++ Rainstorm/Protocols/WeatherData.swift
@@ -0,0 +1,39 @@
+//
+//  WeatherData.swift
+//  Rainstorm
+//
+//  Created by Dennis Vera on 7/20/18.
+//  Copyright © 2018 Dennis Vera. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherData {
+    
+    var latitude: Double { get }
+    var longitude: Double { get }
+    
+    var current: CurrentWeatherConditions { get }
+    var forecast: [WeatherConditions] { get }
+}
+
+protocol WeatherConditions {
+    
+    var time: Date { get }
+    var icon: String { get }
+    var windSpeed: Double { get }
+}
+
+protocol CurrentWeatherConditions: WeatherConditions {
+
+    var summary: String { get }
+    var temperature: Double { get }
+}
+
+protocol ForecastWeatherConditions: WeatherConditions {
+    
+    var temperatureMin: Double { get }
+    var temperatureMax: Double { get }
+}
+
+
